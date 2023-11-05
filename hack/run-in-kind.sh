45f5a359aa523d984c3c7b81c337067cb9333f7c
John Allberg <john@ayoy.se>
2023-11-05 06:13:31 -0600

    Get kind to work (#18)
    * kind: Verify binary commands exist before running script.
    
    * kind: Run helm dependency update before installing argocd.
    
    * kind: Only install sealed-secrets-secret if file exists.
    
    * kind: Upgrade ArgoCD used in kind.
    
    * kind: Set reloader security values.
    
    * kind: User ingressClassName instead of annotation
    
    * cert-manager: Upgrade to 1.13.2.
    
    * cert-manager-config: Allow issuing self-signed CA.

--- hack/run-in-kind.sh
+++ hack/run-in-kind.sh
@@ -3,6 +3,23 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 CLUSTER_NAME=home-cluster
 KUBECONFIG=~/.kube/${CLUSTER_NAME}-argo
 
+echo "Verify binaries exist"
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    exit 1
+fi
+if ! command -v kind &> /dev/null
+then
+    echo "kind could not be found"
+    exit 1
+fi
+if ! command -v kubectl &> /dev/null
+then
+    echo "kubectl could not be found"
+    exit 1
+fi
+
 pushd "${SCRIPT_DIR}" \
   || exit 1
 
@@ -44,10 +61,13 @@ kubectl --kubeconfig "${KUBECONFIG}" \
 
 echo ""
 echo "Install argocd"
+helm dependency update \
+  ./argo-install \
+  || exit 1
 helm --kubeconfig "${KUBECONFIG}" \
-   install -n argocd \
-   argocd ./argo-install \
-   || exit 1
+  install -n argocd \
+  argocd ./argo-install \
+  || exit 1
 
 echo ""
 echo "Wait for argocd to start"
@@ -64,12 +84,15 @@ kubectl \
   --timeout=180s \
   || exit 1
 
-echo ""
-echo "Prepare for sealed-secrets"
-kubectl \
-  --kubeconfig "${KUBECONFIG}" \
-  apply -f "${SCRIPT_DIR}/sealed-secrets-secret.yml" \
-  || exit 1
+SEALED_FILE="${SCRIPT_DIR}/sealed-secrets-secret.yml"
+if test -f "${SEALED_FILE}"; then
+  echo ""
+  echo "Prepare for sealed-secrets"
+  kubectl \
+    --kubeconfig "${KUBECONFIG}" \
+    apply -f "${SEALED_FILE}" \
+    || exit 1
+fi
 
 echo ""
 echo "Install application"
@@ -96,7 +119,7 @@ done
 echo ""
 ARGO_PWD=$(kubectl --kubeconfig "${KUBECONFIG}" \
   -n argocd \
-  get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
+  get secret argocd-secret -o jsonpath="{.data.'admin.password'}" | base64 -d)
 ARGO_HOST=$(kubectl --kubeconfig "${KUBECONFIG}" \
   -n argocd \
   get ingress argocd-server-ingress -o json | jq -r '.spec.rules[0].host')
